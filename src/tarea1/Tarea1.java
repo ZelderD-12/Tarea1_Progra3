e8b984a241e0712b4a2b8e573e79ccf5cd1e0c1a
OmegaKerveus7 <tobiasgusito@outlook.com>
2025-02-02 20:42:47 -0600

    Actulizacion con el archivo main

---
+++ src/tarea1/Tarea1.java
@@ -0,0 +1,12 @@
+package tarea1;
+
+public class Tarea1 {
+
+   
+    public static void main(String[] args) {
+      Frmventanaprincipal ventana1 = new Frmventanaprincipal();
+      ventana1.setLocationRelativeTo(null);
+      ventana1.setVisible(true);
+    }
+    
+}
