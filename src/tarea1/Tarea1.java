146117213947d2bbcec2aa0796a166b5f30b38d0
OmegaKerveus7 <tobiasgusito@outlook.com>
2025-02-02 21:09:08 -0600

    cacomentario main

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -4,6 +4,13 @@ public class Tarea1 {
 
    
     public static void main(String[] args) {
+        //Comentario
+        /*+}+--
+        +
+        ++
+        6+6+6+6+6+9+96+98++98+659+8
+        */
+       
       Frmventanaprincipal ventana1 = new Frmventanaprincipal();
       ventana1.setLocationRelativeTo(null);
       ventana1.setVisible(true);
