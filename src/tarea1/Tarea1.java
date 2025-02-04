2d2d7ce76c715ac2a5bf7b2a89dadec7242e7632
OmegaKerveus7 <tobiasgusito@outlook.com>
2025-02-04 12:56:51 -0600

    integracion y nueva clase para generador de numeros

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -1,13 +1,11 @@
 package tarea1;
 
 public class Tarea1 {
-
-   
+  
     public static void main(String[] args) {
      //Comentarioprueba de mi comentario PRUEBA PARA GUSTAVO .
       Frmventanaprincipal ventana1 = new Frmventanaprincipal();
       ventana1.setLocationRelativeTo(null);
       ventana1.setVisible(true);
-    }
-    
+    }   
 }
