97ddc368798ce1a21e303825a06f3bd9c3e6ebd6
OmegaKerveus7 <tobiasgusito@outlook.com>
2025-02-04 10:22:20 -0600

    clase reproducir

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -4,8 +4,7 @@ public class Tarea1 {
 
    
     public static void main(String[] args) {
-        //Comentarioprueba de mi comentario PRUEBA PARA GUSTAVO 
-        System.out.println("HOLA MUNDO ABIGAIL");
+     //Comentarioprueba de mi comentario PRUEBA PARA GUSTAVO 
       Frmventanaprincipal ventana1 = new Frmventanaprincipal();
       ventana1.setLocationRelativeTo(null);
       ventana1.setVisible(true);
