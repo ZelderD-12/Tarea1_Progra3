c10efe38e1729db9694d60b5f085ead5909e1f66
Diego Castellanos <80775583+ZelderD-12@users.noreply.github.com>
2025-02-05 23:16:02 -0600

    Combinada (#5)
    * PRUEBA DE MUESTRA ABIGAIL
    
    * Disenio
    
    * regreso de la musca sin los archivos
    
    * Diseño 2 con la tabla de contenido
    
    * regreso de la musca sin los archivos
    
    * Actualizacion de la interfaz grafica y la musica
    
    * clase reproducir
    
    * clase reproducir.
    
    * clase reproducir..
    
    * se agregar cancion de elevador
    
    * integracion y nueva clase para generador de numeros
    
    * Creacion de las clases de apoyo para metodos shell y selection
    
    * Adicion de cronometro
    
    ---------
    
    Co-authored-by: diego <diego@Zeldero>
    Co-authored-by: ninet <ninet@Abigail707>
    Co-authored-by: OmegaKerveus7 <tobiasgusito@outlook.com>

--- src/tarea1/Tarea1.java
+++ src/tarea1/Tarea1.java
@@ -1,14 +1,13 @@
 package tarea1;
 
 public class Tarea1 {
-
-   
+  
     public static void main(String[] args) {
-        //Comentarioprueba de mi comentario PRUEBA PARA GUSTAVO 
-       
+     //Comentarioprueba de mi comentario PRUEBA PARA GUSTAVO ..
       Frmventanaprincipal ventana1 = new Frmventanaprincipal();
       ventana1.setLocationRelativeTo(null);
       ventana1.setVisible(true);
-    }
-    
+      
+      
+    }   
 }
