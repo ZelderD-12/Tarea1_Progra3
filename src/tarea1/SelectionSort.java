6daf8b1885079e149bcf8352a50bd41e58e6467f
diego <diego@Zeldero>
2025-02-06 21:38:58 -0600

    SELECTION SORT

--- src/tarea1/SelectionSort.java
+++ src/tarea1/SelectionSort.java
@@ -7,12 +7,24 @@ public class SelectionSort extends Thread{
     @Override
     public void run(){
         long startTime = System.currentTimeMillis();
-     int[] arreglodeprueba = {5,0,4,7,8,33,6,99,4};
-     for(int a = 0; a<=1000000; a++){
-    for(int i = 0; i<= arreglodeprueba.length-1; i++){
-        System.out.println("" + arreglodeprueba[i]);
+     int[] arreglodeprueba = {5,0,4,7,8,33,6,99,4,70,25,66};
+     
+    for(int i = 0; i<arreglodeprueba.length; i++){
+        int min = i;
+        for(int j = i+1; j<arreglodeprueba.length; j++){
+            if(arreglodeprueba[min] > arreglodeprueba[j]){
+                min = j;
+            }
+        }
         
-    }}
+        int temp = arreglodeprueba[i];
+        arreglodeprueba[i] = arreglodeprueba[min];
+        arreglodeprueba[min] = temp;
+    }
+    
+    for(int a = 0; a< arreglodeprueba.length; a++){
+        System.out.println(""+ arreglodeprueba[a]);
+    }
      long endTime = System.currentTimeMillis();
      long milisegundos = endTime - startTime;
      long segundos = 0;
